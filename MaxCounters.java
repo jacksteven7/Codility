b0ad5632541a8f528161446c7c2965157abe13fe
Jack Ibarra <jacksteven7@gmail.com>
2016-07-22 20:09:19 -0500

    Create MaxCounters.java

---
+++ MaxCounters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int N, int[] A) {
+        int[] nums = new int[N];
+        int max=0;
+        int maxCounter = 0;
+        int minCounter = 0;
+        for (int i = 0; i < A.length; i++) {
+        	int num = A[i];
+        	if(num==N+1){
+        		minCounter=maxCounter;
+			}else{
+			  nums[A[i]-1] = Math.max(minCounter+1, nums[A[i]-1] + 1);
+			  maxCounter = Math.max(nums[A[i]-1], maxCounter);
+			}
+        }
+        for (int i = 0; i < nums.length; i++) {
+        	nums[i] = Math.max(nums[i], minCounter);  
+		}
+        return nums;
+    }
+}

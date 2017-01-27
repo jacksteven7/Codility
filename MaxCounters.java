a997160a6a2e290fe43d091d5c197e4f84c4b6a2
Jack Ibarra <jacksteven7@gmail.com>
2017-01-27 17:25:32 -0500

    Update MaxCounters.java

--- MaxCounters.java
+++ MaxCounters.java
@@ -7,15 +7,15 @@ class Solution {
         for (int i = 0; i < A.length; i++) {
         	int num = A[i];
         	if(num==N+1){
-        		minCounter=maxCounter;
-			}else{
-			  nums[A[i]-1] = Math.max(minCounter+1, nums[A[i]-1] + 1);
-			  maxCounter = Math.max(nums[A[i]-1], maxCounter);
-			}
+		  minCounter=maxCounter;
+		}else{
+		  nums[A[i]-1] = Math.max(minCounter+1, nums[A[i]-1] + 1);
+		  maxCounter = Math.max(nums[A[i]-1], maxCounter);
+		}
         }
         for (int i = 0; i < nums.length; i++) {
-        	nums[i] = Math.max(nums[i], minCounter);  
-		}
+	  nums[i] = Math.max(nums[i], minCounter);  
+	}
         return nums;
     }
 }
